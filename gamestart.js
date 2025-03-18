a2d41baea028c124dc297a389bf157c8bfaeef7b
sakitibi <sakitibi10nin@icloud.com>
2025-03-18 18:08:55 +0900

    Update gamestart.js

--- gamestart.js
+++ gamestart.js
@@ -1,5 +1,5 @@
 document.getElementById('home-window button').addEventListener('click', function() {
-    location.href = 'DownLoad';
+    location.href = 'https://github.com/sakitibi/TrainBuildersWeb/releases/download/TBW/TrainBuilders.Web.dmg.zip';
     // Add your game start logic here
     });
 document.getElementById('settings').addEventListener('click', function() {
