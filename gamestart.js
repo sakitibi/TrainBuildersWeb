fd8c1e4b7176e96f8b014b73db9b804cc9a57f1c
sakitibi <sakitibi10nin@icloud.com>
2025-03-18 18:03:35 +0900

    Add files via upload

---
+++ gamestart.js
@@ -0,0 +1,13 @@
+document.getElementById('home-window button').addEventListener('click', function() {
+    location.href = './start.html';
+    // Add your game start logic here
+    });
+document.getElementById('settings').addEventListener('click', function() {
+    location.href = 'https://sakitibi-com9.webnode.jp/page/24/bf921c72-d969-5837-122a-20ba91bad023/';
+    // Add your settings logic here
+});
+
+document.getElementById('exit').addEventListener('click', function() {  
+    alert('Exit Clicked!');
+    // Add your exit logic here
+});
