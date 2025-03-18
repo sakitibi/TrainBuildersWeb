395b3baf0d258a91dfc6c1c472ac5aebf44fbe4c
sakitibi <sakitibi10nin@icloud.com>
2025-03-18 19:19:49 +0900

    Update gamestart.js

--- gamestart.js
+++ gamestart.js
@@ -6,12 +6,3 @@ document.getElementById('settings').addEventListener('click', function() {
     location.href = 'https://sakitibi-com9.webnode.jp/page/24/bf921c72-d969-5837-122a-20ba91bad023/';
     // Add your settings logic here
 });
-
-document.getElementById('exit').addEventListener('click', function() {
-    let slp = Math.floor(Math.random() * 9000 + 1000);
-    alert('TrainBuilders.appを開きますか?').addEventListener('click', function() {
-        slp = Math.floor(Math.random() * 9000 + 1000);
-        alert('読み込み中... 絶対に閉じないで下さい！')
-        sleep(slp);
-        alert('こちらのリンクを開いて下さい file:///TrainBuildersアプリケーションがあるディレクトリのパス/TrainBuilders.app/start.html');
-    });
