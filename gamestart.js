fe192f800e98055bd3ec90fc03b5a70433f7c8d2
さきちび <sakitibi10nin@icloud.com>
2025-04-11 12:56:51 +0900

    Update gamestart.js

--- gamestart.js
+++ gamestart.js
@@ -1,41 +1,13 @@
 document.getElementById('home-window button').addEventListener('click', function() {
     openPopup();
-    // Add your game start logic here
-});
-document.getElementById('start button').addEventListener('click', function() {
-    startGame();
-    // Add your game start logic here
 });
 document.getElementById('settings').addEventListener('click', function() {
     location.href = 'https://sakitibi-com9.webnode.jp/page/24/bf921c72-d969-5837-122a-20ba91bad023/';
-    // Add your settings logic here
 });
 document.getElementById('exit').addEventListener('click', function() {
     location.href = 'https://github.com/sakitibi/TrainBuildersWeb';
-    // Add your settings logic here
 });
 
 function openPopup() {
   window.open("https://sakitibi-com9.webnode.jp/api/trainbuilders/login/24ac77a0-013e-45ae-97d1-b6cc59fb958a/", "popupWindow", "width=800,height=600");
-  window.open("https://sakitibi-com9.webnode.jp/api/trainbuilders/login/24ac77a0-013e-45ae-97d1-b6cc59fb958a/", "popupWindow", "width=800,height=500");
-  window.open("https://sakitibi-com9.webnode.jp/api/trainbuilders/login/24ac77a0-013e-45ae-97d1-b6cc59fb958a/", "popupWindow", "width=800,height=500");
-}
-function startGame() {
-    const xhr = new XMLHttpRequest();
-    var data;
-        
-    window.onload = function onLoad() {
-        xhr.open('GET', 'file:///Applications/TrainBuilders.app/Contents/Resources/.start.html', false); // GETでローカルファイルを開く
-        xhr.onload = () => {
-            data = xhr.responseText;
-        };
-        xhr.onerror = () => {
-            console.log("error!");
-        };
- 
-        xhr.send();
-
-        cts1 = document.getElementById("cts1");
-        cts1.innerText = data;
-    }
 }
