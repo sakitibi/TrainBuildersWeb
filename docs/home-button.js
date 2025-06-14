6a63cebc0c61445122ac117d2ee2b6e88b5f18f9
sakitibi <sakitibi10nin@icloud.com>
2025-06-14 11:32:27 +0900

    Merge remote changes

---
+++ docs/home-button.js
@@ -0,0 +1,7 @@
+document.getElementById("app-edition").addEventListener("click", function(){
+    location.href = "./app";
+});
+
+document.getElementById("web-edition").addEventListener("click", function(){
+    location.href = "./web";
+})
